e061268c1722959bd0c14faf0f906c44b5982d9e
Jay Oster <jay@kodewerx.org>
2020-06-07 16:17:27 -0700

    Add unit tests for the search expression parser

--- src/search/expr.rs
+++ src/search/expr.rs
@@ -6,13 +6,13 @@ use std::io;
 
 lalrpop_mod!(parser, "/search/parser.rs");
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum ValueAst {
     Literal(SignalValue),
     Id(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum ExprAst {
     Equal(String, ValueAst),
     Transition(String, ValueAst),
@@ -39,29 +39,133 @@ impl ExprAst {
 #[cfg(test)]
 mod test {
     use super::*;
+    use crate::signaldb::BitValue;
 
     #[test]
     fn expr() {
-        assert!(parser::ExprParser::new().parse("$a is b0").is_ok());
-        assert!(parser::ExprParser::new().parse("$abc is 42").is_ok());
-        assert!(parser::ExprParser::new().parse("$abc is bu").is_ok());
-        assert!(parser::ExprParser::new().parse("$u is bz").is_ok());
-        assert!(parser::ExprParser::new().parse("$bz is bz").is_ok());
-        assert!(parser::ExprParser::new().parse("bz = bz").is_err());
-        assert!(parser::ExprParser::new().parse("$.* is bz").is_ok());
-        assert!(parser::ExprParser::new().parse("$a becomes 0").is_ok());
-        assert!(parser::ExprParser::new().parse("$a becomes (0)").is_ok());
-        assert!(parser::ExprParser::new().parse("($a becomes (0))").is_ok());
-        assert!(parser::ExprParser::new()
-            .parse("$a <- 0 and $b = 4")
-            .is_ok());
-        assert!(parser::ExprParser::new()
-            .parse("$a <- 0 and after 42")
-            .is_ok());
-        assert!(parser::ExprParser::new()
-            .parse("$a <- 0 and before 42")
-            .is_ok());
-        assert!(parser::ExprParser::new().parse("$a <- 0").is_ok());
-        assert!(parser::ExprParser::new().parse("$a <- $b").is_ok());
+        assert!(ExprAst::from_str("$a is b0").is_ok());
+        assert!(ExprAst::from_str("$abc is 42").is_ok());
+        assert!(ExprAst::from_str("$abc is bu").is_ok());
+        assert!(ExprAst::from_str("$u is bz").is_ok());
+        assert!(ExprAst::from_str("$bz is bz").is_ok());
+        assert!(ExprAst::from_str("bz = bz").is_err());
+        assert!(ExprAst::from_str("$.* is bz").is_ok());
+        assert!(ExprAst::from_str("$a becomes 0").is_ok());
+        assert!(ExprAst::from_str("$a becomes (0)").is_ok());
+        assert!(ExprAst::from_str("($a becomes (0))").is_ok());
+        assert!(ExprAst::from_str("$a <- 0 and $b = 4").is_ok());
+        assert!(ExprAst::from_str("$a <- 0 and after 42").is_ok());
+        assert!(ExprAst::from_str("$a <- 0 and before 42").is_ok());
+        assert!(ExprAst::from_str("$a <- 0").is_ok());
+        assert!(ExprAst::from_str("$a <- $b").is_ok());
+    }
+
+    #[test]
+    fn test_eq() {
+        assert_eq!(
+            ExprAst::from_str("$a is b0").unwrap(),
+            ExprAst::Equal("a".to_string(), ValueAst::Literal(SignalValue::new(0)))
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$abc is 42").unwrap(),
+            ExprAst::Equal("abc".to_string(), ValueAst::Literal(SignalValue::new(42)))
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$abc is bu").unwrap(),
+            ExprAst::Equal("abc".to_string(), ValueAst::Literal(SignalValue::invalid()))
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$u is bz").unwrap(),
+            ExprAst::Equal(
+                "u".to_string(),
+                ValueAst::Literal(SignalValue::new_default(1, BitValue::HighZ)),
+            )
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$bz is bz").unwrap(),
+            ExprAst::Equal(
+                "bz".to_string(),
+                ValueAst::Literal(SignalValue::new_default(1, BitValue::HighZ)),
+            )
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$.* is bz").unwrap(),
+            ExprAst::Equal(
+                ".*".to_string(),
+                ValueAst::Literal(SignalValue::new_default(1, BitValue::HighZ)),
+            )
+        );
+    }
+
+    #[test]
+    fn test_transition() {
+        assert_eq!(
+            ExprAst::from_str("$a becomes 0").unwrap(),
+            ExprAst::Transition("a".to_string(), ValueAst::Literal(SignalValue::new(0)))
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$a becomes (0)").unwrap(),
+            ExprAst::Transition("a".to_string(), ValueAst::Literal(SignalValue::new(0)))
+        );
+
+        assert_eq!(
+            ExprAst::from_str("($a becomes (0))").unwrap(),
+            ExprAst::Transition("a".to_string(), ValueAst::Literal(SignalValue::new(0)))
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$a <- 0").unwrap(),
+            ExprAst::Transition("a".to_string(), ValueAst::Literal(SignalValue::new(0)))
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$a <- $b").unwrap(),
+            ExprAst::Transition("a".to_string(), ValueAst::Id("b".to_string()))
+        );
+    }
+
+    #[test]
+    fn test_condition() {
+        assert_eq!(
+            ExprAst::from_str("$a <- 0 and $b = 4").unwrap(),
+            ExprAst::And(
+                Box::new(ExprAst::Transition(
+                    "a".to_string(),
+                    ValueAst::Literal(SignalValue::new(0)),
+                )),
+                Box::new(ExprAst::Equal(
+                    "b".to_string(),
+                    ValueAst::Literal(SignalValue::new(4)),
+                )),
+            )
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$a <- 0 and after 42").unwrap(),
+            ExprAst::And(
+                Box::new(ExprAst::Transition(
+                    "a".to_string(),
+                    ValueAst::Literal(SignalValue::new(0)),
+                )),
+                Box::new(ExprAst::After(42)),
+            )
+        );
+
+        assert_eq!(
+            ExprAst::from_str("$a <- 0 and before 42").unwrap(),
+            ExprAst::And(
+                Box::new(ExprAst::Transition(
+                    "a".to_string(),
+                    ValueAst::Literal(SignalValue::new(0)),
+                )),
+                Box::new(ExprAst::Before(42)),
+            )
+        );
     }
 }
